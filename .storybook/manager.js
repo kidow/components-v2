633884afb4b926b052e53944b8db1de00dfca647
kidow <wcgo2ling@gmail.com>
2022-12-16 11:28:07 +0900

    brand logo 변경

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,2 +1,15 @@
+import { addons } from '@storybook/addons'
+import { create } from '@storybook/theming'
+
 window.STORYBOOK_GA_ID = 'G-3VHNBFM0PK'
 window.STORYBOOK_REACT_GA_OPTIONS = {}
+
+addons.setConfig({
+  theme: create({
+    base: 'light',
+    brandTitle: 'Kidow',
+    brandUrl: 'https://components.kidow.me',
+    brandImage: 'https://files.kidow.me/logos/storybook.svg',
+    brandTarget: '_blank'
+  })
+})
