c3d4498e35db3479801f1ac9ddd9cd7cc14fc1d0
kidow <wcgo2ling@gmail.com>
2022-12-18 23:03:59 +0900

    Tooltip

--- src/stories/Tooltip/index.stories.tsx
+++ src/stories/Tooltip/index.stories.tsx
@@ -3,9 +3,28 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
   title: 'Data Display/Tooltip',
-  component: Component
+  component: Component,
+  argTypes: {
+    content: {
+      control: { type: 'string' },
+      defaultValue: 'content'
+    },
+    position: {
+      control: { type: 'radio' },
+      options: ['top', 'right', 'bottom', 'left'],
+      defaultValue: 'top'
+    },
+    arrow: {
+      control: { type: 'boolean' },
+      defaultValue: true
+    }
+  }
 } as ComponentMeta<typeof Component>
 
 export const Default: ComponentStory<typeof Component> = ({ ...props }) => (
-  <Component {...props} />
+  <div className="flex h-24 items-center justify-center">
+    <Component {...props}>
+      <span>Tooltip</span>
+    </Component>
+  </div>
 )
