3766caa75e0a465862d2ac24d697a1c0bafb2e71
kidow <wcgo2ling@gmail.com>
2022-12-21 12:43:43 +0900

    Toast

--- src/stories/Toast/index.stories.tsx
+++ src/stories/Toast/index.stories.tsx
@@ -1,5 +1,26 @@
 import Component from '.'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
+import { EventListener } from 'services'
+
+class Toast {
+  private emit(message: string, type: NToast.Type) {
+    EventListener.emit<NToast.Emit>('toast', { message, type })
+  }
+  success(message: string) {
+    this.emit(message, 'success')
+  }
+  info(message: string) {
+    this.emit(message, 'info')
+  }
+  warn(message: string) {
+    this.emit(message, 'warn')
+  }
+  error(message: string) {
+    this.emit(message, 'error')
+  }
+}
+
+const toast = new Toast()
 
 export default {
   title: 'Feedback/Toast',
@@ -7,5 +28,13 @@ export default {
 } as ComponentMeta<typeof Component>
 
 export const Default: ComponentStory<typeof Component> = ({ ...props }) => (
-  <Component {...props} />
+  <>
+    <div className="flex gap-3">
+      <button onClick={() => toast.success('Success')}>Success</button>
+      <button onClick={() => toast.info('Info')}>Info</button>
+      <button onClick={() => toast.warn('Warn')}>Warn</button>
+      <button onClick={() => toast.error('Error')}>Error</button>
+    </div>
+    <Component {...props} />
+  </>
 )
